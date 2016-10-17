40c3c240e8ae61b467ff2397eb5fd88dcfb483eb
risju62 <Risto@IsannanKone>
2016-10-17 17:24:12 +0300

    Done

--- src/fi/oulu/tol/sqat/GildedRose.java
+++ src/fi/oulu/tol/sqat/GildedRose.java
@@ -1,98 +1,146 @@
 package fi.oulu.tol.sqat;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 
 public class GildedRose {
 
-	private static List<Item> items = null;
+	private static ArrayList<Item> items = null;
 
-	public List<Item> getItems() {
-		return items;
-	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+        System.out.println("OMGHAI!");
+		
+        items = new ArrayList<Item>();
+        items.add(new Item("+5 Dexterity Vest", 10, 20));
+        items.add(new Item("Aged Brie", 2, 0));
+        items.add(new Item("Elixir of the Mongoose", 5, 7));
+        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        items.add(new Item("Conjured Mana Cake", 3, 6));
+
+        updateQuality();
+}
+
+
 	
-	public void addItem(Item item) {
-		items.add(item);
-	}
-
-	public GildedRose() {
-		items = new ArrayList<Item>();
-	}
-    public static void updateEndOfDay()
+    public static void updateQuality()
     {
-        for (int i = 0; i < items.size(); i++)
+        //for (int i = 0; i < items.size(); i++)
+    	
+    	for(Item item:items)
         {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
+    		 if (("Aged Brie".equals(item.getName())) || "Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
             {
-                if (items.get(i).getQuality() > 0)
-                {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - 1);
-                    }
-                }
+                 if (item.getQuality() < 50)
+                 {
+                     item.increaseQuality();
+
+                     if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+                     {
+                         if (item.getSellIn() < 11)
+                         {
+                             if (item.getQuality() < 50)
+                             {
+                                 item.increaseQuality(); 
+                             }
+                         }
+
+                         if (item.getSellIn() < 6)
+                         {
+                             if (item.getQuality() < 50)
+                             {
+                                 item.increaseQuality();
+                             }
+                         }
+                     }
+                 }
             }
             else
             {
-                if (items.get(i).getQuality() < 50)
+                if (item.getQuality() > 0)
+                	
                 {
-                    items.get(i).setQuality(items.get(i).getQuality() + 1);
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
+                    if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
                     {
-                        if (items.get(i).getSellIn() < 11)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-
-                        if (items.get(i).getSellIn() < 6)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
+                        item.decreaseQuality();
                     }
                 }
             }
 
-            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
+            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
             {
-                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
+                item.decreaseSellIn( );
             }
 
-            if (items.get(i).getSellIn() < 0)
+            if (item.getSellIn() < 0)
             {
-                if (!"Aged Brie".equals(items.get(i).getName()))
+                if ("Aged Brie".equals(item.getName()))
                 {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
+
+                	if (item.getQuality() < 50)
                     {
-                        if (items.get(i).getQuality() > 0)
-                        {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() - 1);
-                            }
-                        }
+                       item.increaseQuality();
                     }
-                    else
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-                    }
+                
                 }
                 else
                 {
-                    if (items.get(i).getQuality() < 50)
+                	if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
                     {
-                        items.get(i).setQuality(items.get(i).getQuality() + 1);
+                        item.setQuality(item.getQuality() - item.getQuality());                    }
+                    else
+                    {
+                        if (item.getQuality() > 0)
+                        {
+                            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+                            {
+                                item.decreaseQuality();
+                            }
+                        }
+
                     }
                 }
             }
         }
     }
+
+
+
+	public void addItem(Item item) {
+		// TODO Auto-generated method stub
+	 
+		
+		try {
+			items.add(item);
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			
+			items = new ArrayList<Item>();
+			items.add(item);
+		}
+		
+	}
+
+
+
+	public void updateEndOfDay() {
+		// TODO Auto-generated method stub
+		updateQuality();
+		
+	}
+
+
+
+	public ArrayList<Item> getItems() {
+		// TODO Auto-generated method stub
+		ArrayList <Item> rItems =items;
+		Collections.reverse(rItems);
+		return rItems;
+	}
 
 }
